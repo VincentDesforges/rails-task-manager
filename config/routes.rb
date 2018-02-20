2fa111c2285e9f1106896bb71a5090ac67584ac1
Vincent Desforges <desforges.vincent@gmail.com>
2018-02-20 14:26:37 +0000

    completed the task manager app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Read all
+  get "tasks", to: "tasks#index"
+  # find one
+  get "task/:id", to: "tasks#show", as: "task"
+  # create new
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+  # update
+  get "task/:id/edit", to: "tasks#edit", as: "task_update"
+  patch "task/:id", to: "tasks#update"
+  # delete
+  delete "task/:id", to: "tasks#destroy"
 end
